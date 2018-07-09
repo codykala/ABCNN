a1b67cb66400d74565092cf5d61b3dcabd6d345f
Cody Keola Kala <cody@moveworks.ai>
2018-07-08 17:45:02 -0700

    fleshed out main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # coding=utf-8
 
+import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+from collections import defaultdict
+
+from data.dataset import QuestionDataset
 from setup import read_config
 from setup import setup_model
 from train import train
@@ -8,9 +14,22 @@ from train import train
 config = read_config("config.json")
 model, index2word, word2index = setup_model(config)
 
+# Create the loss function
+loss_fn = nn.BCEWithLogitsLoss()
+
+# Create the optimizer
+optimizer = optim.Adam(model.parameters())
+
+# Create metrics dict
+metrics = {}    # TODO: Implement custom metrics functions to track
+
+# Create history dict
+history = defaultdict(list)
+
 # Load the data
-# TODO: Write functions to read in the training and validation datasets
+trainset = QuestionDataset("data/quora/train.csv", index2word, word2index)
+valset = QuestionDataset("data/quora/val.csv", index2word, word2index)
 
 # Train the model
-model = train(model)
+model = train(model, loss_fn, optimizer, metrics, history, trainset, valset, config["train"])
 
