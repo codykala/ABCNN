3713e68b07c3c815c5773568ac1415136c238ec5
Cody Keola Kala <cody@moveworks.ai>
2018-07-08 17:46:46 -0700

    implemented save/loading checkpoints and generating plots.

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 
 import torch
-import torch.nn as nn
+import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend("agg")  
 
-def save_checkpoint(model, optimizer, history, epoch):
+def save_checkpoint(model, optimizer, history, epoch, filename):
     """ Saves the state of the model to a pickle file so that it can continue 
         to be trained at a later time.
 
@@ -22,14 +22,15 @@ def save_checkpoint(model, optimizer, history, epoch):
         Returns:
             None
     """
-    pass
+    state = (model.state_dict(), optimizer.state_dict(), history, epoch)
+    torch.save(state, filename)
 
 
-def load_checkpoint(checkpoint_path):
+def load_checkpoint(filename):
     """ Loads a model from a pickled checkpoint file.
 
         Args:
-            checkpoint_path: string
+            filename: string
                 The path to the checkpoint file.
 
         Returns
@@ -42,11 +43,12 @@ def load_checkpoint(checkpoint_path):
             epoch: int
                 The current epoch number.
     """
-    pass
+    state = torch.load(filename)
+    return state
     
 
 def generate_plots(history):
-    """ Creates plots of the metrics.
+    """ Creates plots of the metric values stored in history.
     
         Args:
             history: dict
@@ -55,4 +57,11 @@ def generate_plots(history):
         Returns:
             None
     """
-    pass
+    for metric, vals in history.iteritems():
+       time = np.arange(1, len(vals) + 1)
+       plt.scatter(time, vals, marker='x', color='red')
+       plt.xlabel("time")
+       plt.ylabel(metric)
+       plt.title("{} vs time".format(metric))
+       plt.savefig("{}_epoch_{}.png".format(metric, len(vals)))
+       plt.clf() 
